b8fbb6f3e010645b54f67841dc38ec5932262f2d
M Haris <harisabbasra19na@gmail.com>
2026-04-01 12:02:46 +0500

    fix the issues

--- index.js
+++ index.js
@@ -35,7 +35,11 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-//     console.log(`API Docs available at http://localhost:${PORT}/api-docs`);
-// });
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+        console.log(`API Docs available at http://localhost:${PORT}/api-docs`);
+    });
+}
+
+module.exports = app;
