352deb85a8ceefc3d692764a96a56f4ad425f736
M Haris <harisabbasra19na@gmail.com>
2026-04-01 10:31:07 +0500

    moved index.js to api folder for vercel

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    console.log(`API Docs available at http://localhost:${PORT}/api-docs`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+//     console.log(`API Docs available at http://localhost:${PORT}/api-docs`);
+// });
