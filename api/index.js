7dffdc2e1edb5740a43df35b97bb5b4a1294a94d
M Haris <harisabbasra19na@gmail.com>
2026-04-01 10:51:42 +0500

    fix module system

--- api/index.js
+++ api/index.js
@@ -1,13 +1,20 @@
-import express from "express";
-import cors from "cors";
+
+const express = require("express");
+const cors = require("cors");
+
+const userRoutes = require("../routes/userRoutes");
+const taskRoutes = require("../routes/taskRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/users", userRoutes);
+app.use("/api/tasks", taskRoutes);
+
 app.get("/", (req, res) => {
   res.send("API is working");
 });
 
-export default app;
+module.exports = app;
